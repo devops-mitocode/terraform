ab36561b04fcaaab08614945ff6f842526708b4c
dany.cenas <dany.cenas@bbva.com>
2024-02-24 20:59:59 -0500

    .

--- resources.tf
+++ resources.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "ansible_host_debian" {
+resource "aws_instance" "debian" {
   ami           = "ami-0c2644caf041bb6de"
   instance_type = "t2.medium"
 
   key_name               = "aws-keypair"
-  vpc_security_group_ids = [aws_security_group.ansible_host_debian_sg.id]
+  vpc_security_group_ids = [aws_security_group.debian_sg.id]
   tags = {
-    Name        = "ansible_host_debian"
+    Name        = "debian"
     Environment = "test"
   }
 }
 
-resource "aws_security_group" "ansible_host_debian_sg" {
-  name        = "ansible_host_debian-sg"
+resource "aws_security_group" "debian_sg" {
+  name        = "debian-sg"
   description = "Security group for Jenkins"
 
   ingress {
@@ -30,5 +30,5 @@ resource "aws_security_group" "ansible_host_debian_sg" {
 }
 
 output "ec2_instance_ip" {
-  value = aws_instance.ansible_host_debian.public_ip
+  value = aws_instance.debian.public_ip
 }
