555923b49480cb54082c8b0d68a27a730f913d5c
dany.cenas <dany.cenas@bbva.com>
2024-07-09 21:01:39 -0500

    .

--- resources.tf
+++ resources.tf
@@ -1,20 +1,29 @@
-resource "aws_instance" "debian" {
-  ami           = "ami-0c2272b2da6755fab"
+# resource "aws_instance" "debian" {
+#   ami           = "ami-0c2272b2da6755fab"
+#   instance_type = "t2.micro"
+#   key_name      = "aws-keypair"
+
+#   tags = {
+#     Name        = "debian"
+#     Environment = "calidad"
+#   }
+# }
+
+# resource "aws_instance" "windows" {
+#   ami                    = "ami-07e278fe6c43b6aba"
+#   instance_type          = "t2.micro"
+#   key_name               = "aws-keypair"
+#   tags = {
+#     Name = "windows"
+#   }
+# }
+
+resource "aws_instance" "centos" {
+  ami           = "ami-070c0059c28d778a6"
   instance_type = "t2.micro"
   key_name      = "aws-keypair"
 
   tags = {
-    Name        = "debian"
-    Environment = "calidad"
+    Name        = "centos"
   }
 }
-
-resource "aws_instance" "windows" {
-  ami                    = "ami-07e278fe6c43b6aba"
-  instance_type          = "t2.micro"
-  key_name               = "aws-keypair"
-  tags = {
-    Name = "windows"
-  }
-}
-
