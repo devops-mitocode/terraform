c3c6ad04e4d29e62ccba45b0181f50648bd720fe
dany.cenas <dany.cenas@bbva.com>
2024-09-17 21:16:09 -0500

    .

--- resources.tf
+++ resources.tf
@@ -33,6 +33,13 @@ resource "aws_security_group" "debian_sg" {
   }
 
   ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
