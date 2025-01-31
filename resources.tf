110037437202332ead3c44a4c8f8404b8dd74b47
dany.cenas <dany.cenas@bbva.com>
2025-01-31 00:42:55 -0500

    -

--- resources.tf
+++ resources.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "linux" {
   ami           = "ami-03e383d33727f4804"
   instance_type = "t2.medium"
+
   key_name      = "aws-keypair"
 
   vpc_security_group_ids = [aws_security_group.jenkins-nodes-sg.id]
@@ -13,6 +14,7 @@ resource "aws_instance" "linux" {
 resource "aws_instance" "windows" {
   ami           = "ami-00aba64d12d376282"
   instance_type = "t3.medium"
+  
   key_name = "aws-keypair"
 
   vpc_security_group_ids = [aws_security_group.jenkins-nodes-sg.id]
@@ -45,4 +47,12 @@ resource "aws_security_group" "jenkins-nodes-sg" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+}
+
+output "aws_instance_linux_public_ip" {
+  value = aws_instance.linux.public_ip
+}
+
+output "aws_instance_windows_public_ip" {
+  value = aws_instance.windows.public_ip
 }
